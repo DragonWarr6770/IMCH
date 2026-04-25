02f426d41f91ddfd7d5418db5bbc14fea8a6cdf6
DragonWarr6770 <dragonwarrior0044778@gmail.com>
2026-04-25 16:29:11 +0530

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Slidable BG Chat",
+  description: "Uploadable background with chat",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
